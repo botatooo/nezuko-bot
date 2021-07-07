f3be3f07dbf6b4bb54cc48e5fe452ddee4945b85
botato <botatostuff@gmail.com>
2021-07-07 15:35:01 -0400

    Add status and activity commands

--- Cogs/bot_admin.py
+++ Cogs/bot_admin.py
@@ -7,18 +7,19 @@ class BotAdminCog(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @commands.command(name="say",
-                      aliases=["send", "echo"],
-                      usage="<message>",
-                      description="Make me say something")
+    @commands.command(name='say',
+                      aliases=['send', 'echo'],
+                      usage='<message>',
+                      description='Make me say something')
     async def say(self, ctx: commands.Context, *, message: str):
         allowed_mentions = discord.AllowedMentions(everyone=False, roles=False)
         await ctx.send(message, allowed_mentions=allowed_mentions)
+        await ctx.message.delete(delay=2)
 
-    @commands.command(name="dm",
+    @commands.command(name='dm',
                       aliases=['pm'],
-                      usage="<member> <message>",
-                      description="Make me send a dm to someone.")
+                      usage='<member> <message>',
+                      description='Make me send a dm to someone.')
     @commands.is_owner()
     async def dm(self, ctx: commands.Context, member: discord.Member, *,
                  message: str):
@@ -27,7 +28,43 @@ class BotAdminCog(commands.Cog):
         embed.set_footer(text=f'Sent to {member}')
 
         await member.send(message)
-        await ctx.send(embed=embed)
+        await ctx.reply(embed=embed)
+
+    @commands.command(name='status',
+                      aliases=['setstatus', 'set_status'],
+                      usage='<online|dnd|idle|invisible>',
+                      description='Set the current status to something.')
+    @commands.is_owner()
+    async def status(self, ctx: commands.Context, status: str):
+        if status.lower() in [
+                'dnd', 'do_not_disturb', 'idle', 'invisible', 'offline',
+                'online'
+        ]:
+            await self.client.change_presence(
+                status=getattr(discord.Status, status.lower()))
+        else:
+            await ctx.reply('That is not a status.')
+
+    @commands.command(
+        name='activity',
+        aliases=['setactivity', 'set_activity'],
+        usage='<playing|listening|watching|competing> <name> [url]',
+        description='Set the current activity to something.')
+    @commands.is_owner()
+    async def activity(self, ctx: commands.Context, activity: str, name: str):
+        if activity.lower() in [
+                'playing',
+                'listening',
+                'watching',
+                'competing',
+        ]:
+            await self.client.change_presence(activity=discord.Activity(
+                name=name, type=getattr(discord.ActivityType, activity.lower()))
+                                             )
+        elif activity.lower() == 'streaming':
+            await ctx.reply('Streaming not supported.')
+        else:
+            await ctx.reply('That is not an activity.')
 
 
 def setup(client: commands.Bot):
