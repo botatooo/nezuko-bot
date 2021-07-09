dbcb2e58719c0ec3f6465fc29479a1350b164cbd
botato <botatostuff@gmail.com>
2021-07-08 23:03:57 -0400

    fix this sad excuse of a hack

--- Cogs/commands/moderation.py
+++ Cogs/commands/moderation.py
@@ -3,7 +3,10 @@ from discord import utils
 from discord.ext import commands
 
 import os, sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(
+    0,
+    os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__)))))
 
 from utils.checks import is_bot_admin_or_has_perm  # pylint: disable=import-error
 
--- Cogs/commands/role_management.py
+++ Cogs/commands/role_management.py
@@ -2,7 +2,10 @@ import discord
 from discord.ext import commands
 
 import os, sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(
+    0,
+    os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__)))))
 
 from utils.checks import is_bot_admin_or_has_perm  # pylint: disable=import-error
 
